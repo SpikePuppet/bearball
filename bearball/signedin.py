3f708455372c813726f109122e56add8c1f21d7e
Rhys Johns <rhysjohnsdev@gmail.com>
2025-07-02 22:37:08 -0700

    Setup blueprints

---
+++ bearball/signedin.py
@@ -0,0 +1,3 @@
+from flask import (Blueprint)
+
+bp = Blueprint('signedin', __name__, url_prefix='/games')
---
+++ bearball/signedout.py
@@ -0,0 +1,3 @@
+from flask import (Blueprint)
+
+bp = Blueprint('signedout', __name__, url_prefix='/games')
